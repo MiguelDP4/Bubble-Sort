1584430cd9418d4f7e2f47bedc3a54d7afa0c9e3
MiguelDP4 <migueldp4@gmail.com>
2020-02-20 11:34:38 -0600

    bubble_sort mehod functional

--- bubble.rb
+++ bubble.rb
@@ -2,7 +2,6 @@ def swap(val1, val2)
   temp = val1
   val1 = val2
   val2 = temp
-  puts "swapped"
 end
 
 def bubble_sort(array)
@@ -11,17 +10,22 @@ def bubble_sort(array)
   not_sorted = true
   while ((i < array.length - 1) && not_sorted)
       not_sorted = false
+      j = 0
       while j < array.length - 1
-        if array[i] > array[i+1]
-          swap(array[i], array[i+1])
+        if array[j].to_i > array[j+1].to_i
+          temp = array[j]
+          array[j] = array[j+1]
+          array[j+1] = temp
+          print array
+          puts " "
           not_sorted = true
         end
       j += 1
     end
   i += 1
-end
+  end
 end
 
-test_array = [5,8,3,4,6,2,4,15,2,23]
+test_array = [7,5,8,3,4,6,2,4,15,2,23]
 bubble_sort(test_array)
 print test_array
