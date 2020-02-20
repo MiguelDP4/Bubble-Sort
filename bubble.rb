e4fdebd9ac5fe5e4bedd54750094d8daed99085b
bren2102 <brb.yg.21@gmail.com>
2020-02-20 14:47:52 -0500

    fix linter issues

--- bubble.rb
+++ bubble.rb
@@ -2,57 +2,57 @@ def bubble_sort(array)
   i = 0
   j = 0
   not_sorted = true
-  while ((i < array.length - 1) && not_sorted)
-      not_sorted = false
-      j = 0
+  while i < array.length - 1 && not_sorted
+    not_sorted = false
+    j = 0
       while j < array.length - 1
-        if array[j].to_i > array[j+1].to_i
+        if array[j].to_i > array[j + 1].to_i
           temp = array[j]
-          array[j] = array[j+1]
-          array[j+1] = temp
+          array[j] = array[j + 1]
+          array[j + 1] = temp
           not_sorted = true
         end
-      j += 1
-    end
-  i += 1
+        j += 1
+      end
+    i += 1
   end
 end
 
-def bubble_sort_by(array)  
-  i=0
-  j=0
+def bubble_sort_by(array)
+  i = 0
+  j = 0
 
   not_sorted = true
-  while ((i < array.length - 1) && not_sorted)
-      not_sorted = false
-      j = 0
+  while i < array.length - 1 && not_sorted
+    not_sorted = false
+    j = 0
       while j < array.length - 1
-        comparison = yield(array[j],array[j+1])
-        if comparison>0
+        comparison = yield(array[j], array[j + 1])
+        if comparison.positive?
           temp = array[j]
-          array[j] = array[j+1]
-          array[j+1] = temp
+          array[j] = array[j + 1]
+          array[j + 1] = temp
           not_sorted = true
         end
-      j += 1
-    end
-  i += 1
+        j += 1
+      end
+    i += 1
   end
   print array
 end
 
 int_array = [7,5,8,3,4,6,2,4,15,2,23]
-print "Unordered integer array: "
+print 'Unordered integer array: '
 print int_array
-puts " "
+puts ' '
 bubble_sort(int_array)
-print "Orderede integer array: "
+print 'Orderede integer array: '
 print int_array
-puts " "
+puts ' '
 
-string_array = ["hi","hello","hey", "another_string", "haloo", "testing"]
-print "Unordered string array: "
+string_array = ['hi','hello','hey', 'another_string', 'haloo', 'testing']
+print 'Unordered string array: '
 print string_array
-bubble_sort_by(string_array) {|string1,string2| string1.length-string2.length}
-print "Ordered string array: "
+bubble_sort_by(string_array) { |string1, string2| string1.length - string2.length }
+print 'Ordered string array: '
 print string_array
