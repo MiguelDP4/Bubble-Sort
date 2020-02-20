b29bf55124fc39f52d5718995628343d908f7a3a
MiguelDP4 <migueldp4@gmail.com>
2020-02-20 11:25:49 -0600

    Correct bubble_sort method

--- bubble.rb
+++ bubble.rb
@@ -2,6 +2,7 @@ def swap(val1, val2)
   temp = val1
   val1 = val2
   val2 = temp
+  puts "swapped"
 end
 
 def bubble_sort(array)
@@ -11,13 +12,16 @@ def bubble_sort(array)
   while ((i < array.length - 1) && not_sorted)
       not_sorted = false
       while j < array.length - 1
-      if array[i] > array[i+1]
-        swap(array[i], array[i+1])
-        not_sorted = true
-      end
+        if array[i] > array[i+1]
+          swap(array[i], array[i+1])
+          not_sorted = true
+        end
       j += 1
     end
-    i += 1
-  end
+  i += 1
+end
 end
 
+test_array = [5,8,3,4,6,2,4,15,2,23]
+bubble_sort(test_array)
+print test_array
