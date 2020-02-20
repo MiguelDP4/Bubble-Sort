a453af95c362682aa1545ff533aa078d8b076898
bren2102 <brb.yg.21@gmail.com>
2020-02-20 13:06:06 -0500

    add bubble_sort_by method

--- bubble.rb
+++ bubble.rb
@@ -20,6 +20,34 @@ def bubble_sort(array)
   end
 end
 
-test_array = [7,5,8,3,4,6,2,4,15,2,23]
-bubble_sort(test_array)
-print test_array
+def bubble_sort_by(array)  
+  i=0
+  j=0
+
+  not_sorted = true
+  while ((i < array.length - 1) && not_sorted)
+      not_sorted = false
+      j = 0
+      while j < array.length - 1
+        comparison = yield(array[j],array[j+1])
+        if comparison>0
+          temp = array[j]
+          array[j] = array[j+1]
+          array[j+1] = temp
+          print array
+          puts " "
+          not_sorted = true
+        end
+      j += 1
+    end
+  i += 1
+  end
+  print array
+end
+
+bubble_sort_by(["hi","hello","hey"]) {|string1,string2| string1.length-string2.length}
+
+
+#test_array = [7,5,8,3,4,6,2,4,15,2,23]
+#bubble_sort(test_array)
+#print test_array
